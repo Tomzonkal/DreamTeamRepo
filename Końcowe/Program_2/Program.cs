3700f846bb046e59046d5cc0886955ad3fff55e9
unknown <tomasz.jazdzewski@bluedatabay.pl>
2020-01-18 12:49:15 +0100

    Added all file in one folder

---
+++ Końcowe/Program_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Dictionary<string,string> functions = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+            Reader();
+            Connections();
+        }
+
+        private static void Connections()
+        {
+            string regex = @"([a-zA-Z])+\s[a-zA-Z]+\s*\(.*\)\s*\{";
+            using (StreamReader file = new StreamReader("test.h"))
+            {
+                var x = file.ReadLine();
+                var y = functions.Keys;
+                for(int i=0;i<y.Count;i++)
+                {
+                   
+                }
+
+                file.Close();
+            }
+        }
+
+        static void Reader()
+        {
+            string line = null;
+            string regex = @"([a-zA-Z])+\s[a-zA-Z]+\s*\(.*\)\s*\{";
+            using (StreamReader file = new StreamReader("test.h"))
+            {
+
+                line = file.ReadToEnd();
+                int l = 0;
+                var x = (Regex.Matches(line, regex));
+                l += x.Count;
+                foreach (Match temp in x)
+                {
+                    if(!functions.ContainsKey(temp.Value.Split(" ")[1].Split("(")[0]))
+                    functions.Add(temp.Value.Split(" ")[1].Split("(")[0], "0");
+                }
+
+                file.Close();
+            }
+        }
+    }
+}
---
+++ Końcowe/Program_3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+namespace Modules_realtionsship
+{
+    class node
+    {
+        public string name;
+        public Dictionary<string, int> functions = new Dictionary<string, int>();
+        public int liczba_odwołań = 0;
+        public string context;
+        public string odwolywujący = "";
+    }
+    class Program
+    {
+
+        static List<node> list = new List<node>();
+        static Dictionary<string, int> Reader(string tekst)
+        {
+            string line = null;
+            string regex = @"([a-zA-Z])+\s[a-zA-Z]+\s*\(.*\)\s*\{";
+            Dictionary<string, int> functions = new Dictionary<string, int>();
+            {
+
+                line = tekst;
+                int l = 0;
+                var x = (Regex.Matches(line, regex));
+                l += x.Count;
+                foreach (Match temp in x)
+                {
+
+                    functions.Add((temp.Value.Split(" ")[1] + '('), 0);
+                }
+
+                return functions;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            string[] namespacess;
+            using (StreamReader file = new StreamReader("test.h"))
+            {
+                namespacess = file.ReadToEnd().Split("namespace");
+                file.Close();
+                int i = 0;
+                foreach (var VARIABLE in namespacess)
+                {
+                    if (i != 0)
+                        list.Add(new node() { name = VARIABLE.Split('\n')[0], functions = Reader(VARIABLE), context = VARIABLE });
+                    i++;
+                }
+
+            }
+
+            foreach (var node in list)
+            {
+                foreach (var node_function in list)
+                {
+                    foreach (var function in node_function.functions.Keys.ToList())
+                    {
+                        if (node.context.Contains(function))
+                        {
+                            node_function.functions[function]++;
+                            node_function.liczba_odwołań++;
+                            node_function.odwolywujący += (node.name + '/');
+                        }
+                    }
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("ext.txt"))
+            {
+                writer.Write(JsonConvert.SerializeObject(list));
+            }
+        }
+    }
+}
